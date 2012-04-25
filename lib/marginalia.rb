0110a04a192f7474de9c667a9a373a540b58facd
Noah Lorang <noah@datarobots.com>
2012-04-25 09:59:31 -0400

    Check that the execute method is defiend before attempting to alias it, ref #2

--- lib/marginalia.rb
+++ lib/marginalia.rb
@@ -8,10 +8,12 @@ module Marginalia
       if defined? Rails.application
         Marginalia.application_name = Rails.application.class.name.split("::").first
       end
-      instrumented_class.class_eval do
-        alias_method :execute_without_marginalia, :execute
-        alias_method :execute, :execute_with_marginalia
-      end
+        instrumented_class.class_eval do
+          if defined? :execute
+            alias_method :execute_without_marginalia, :execute
+            alias_method :execute, :execute_with_marginalia
+          end
+        end
     end
 
     def execute_with_marginalia(sql, name = nil)
