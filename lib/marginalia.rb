5d130bf1d77ccf84a4b597ef3d2c53659c5fd058
Noah Lorang <noah@datarobots.com>
2013-03-16 14:04:17 -0400

    Prevent double instrumentation
    Some adapters (MySQL most notably) call execute from exec_query,
    which results in a double comment being added. Check if a comment is
    already appended before annotating with a duplicate.

--- lib/marginalia.rb
+++ lib/marginalia.rb
@@ -22,7 +22,7 @@ module Marginalia
 
     def annotate_sql(sql)
       comment = Marginalia::Comment.construct_comment
-      if comment.present?
+      if comment.present? && !sql.match(comment)
         "#{sql} /*#{comment}*/"
       else
         sql
