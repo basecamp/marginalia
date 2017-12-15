69b4a20d782dcab72091a59fae95a3acef4a4810
Igor Wiedler <igor@travis-ci.org>
2017-12-15 14:48:14 +0100

    patch for older minitest

--- test/escape_test.rb
+++ test/escape_test.rb
@@ -5,6 +5,9 @@ require 'mocha/test_unit'
 
 require 'marginalia'
 
+# Shim for compatibility with older versions of MiniTest
+MiniTest::Test = MiniTest::Unit::TestCase unless defined?(MiniTest::Test)
+
 class EscapeTest < MiniTest::Test
   def test_good_comment
     assert_equal Marginalia.escape_sql_comment('app=foo'), 'app=foo'
