497eaa09ddcb692e758fde1c5f1e006c041c044e
Igor <igorwwwwwwwwwwwwwwwwwwww@users.noreply.github.com>
2018-09-21 14:54:51 +0200

    perform allocation conditionally

--- lib/marginalia/comment.rb
+++ lib/marginalia/comment.rb
@@ -31,7 +31,9 @@ module Marginalia
     end
 
     def self.escape_sql_comment(str)
-      str = str.dup
+      if str.include?('/*') || str.include?('*/')
+        str = str.dup
+      end
       while str.include?('/*') || str.include?('*/')
         str = str.gsub('/*', '').gsub('*/', '')
       end
